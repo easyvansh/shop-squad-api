15b5137262564084934db7fb4c85275333887143
Vansh Singh <vansh2@ualberta.ca>
2023-08-14 12:12:43 +0530

    Initialized API

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const productRoutes = require("./router/productRoutes");
+const orderRoutes = require("./router/orderRoutes");
+const paymentRoutes = require("./router/paymentRoutes");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 3000;
+let cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/payments", paymentRoutes);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log("API is listening on port ", PORT);
+});
