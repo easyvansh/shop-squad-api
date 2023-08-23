25b872bb22a3883fe7b43087f8a527117024eebc
Vansh Singh <vansh2@ualberta.ca>
2023-08-23 11:39:08 +0530

    Updated Landing Page

--- src/index.js
+++ src/index.js
@@ -15,8 +15,49 @@ app.use("/orders", orderRoutes);
 app.use("/users", userRoutes);
 app.use("/payments", paymentRoutes);
 
+// Define the main route with modern styling and a short description
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  const mainPageHtml = `
+    <html>
+    <head>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          margin: 0;
+          padding: 0;
+          background-color: #f4f4f4;
+        }
+        h1 {
+          color: #333;
+          padding: 20px;
+        }
+        ul {
+          list-style: none;
+          padding: 0;
+        }
+        li {
+          margin: 10px;
+        }
+        a {
+          text-decoration: none;
+          color: #007bff;
+          font-weight: bold;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Welcome to the Shop-Squad App Server</h1>
+      <p>This server serves as the backend for the Shop-Squad App, an ecommerce platform with a group buy feature. Below are the available routes:</p>
+      <ul>
+        <li><a href="/products">Products</a> - Manage available products</li>
+        <li><a href="/orders">Orders</a> - Handle user orders</li>
+        <li><a href="/users">Users</a> - Manage user accounts</li>
+        <li><a href="/payments">Payments</a> - Process payments</li>
+      </ul>
+    </body>
+    </html>
+  `;
+  res.send(mainPageHtml);
 });
 
 app.listen(PORT, () => {
