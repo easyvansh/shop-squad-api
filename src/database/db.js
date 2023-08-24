6030759a4c2c01f77862ececfa408f8bebdf35aa
Vansh Singh <vansh2@ualberta.ca>
2023-08-24 16:53:31 +0530

    Updated Cart Route

--- src/database/db.js
+++ src/database/db.js
@@ -3,11 +3,13 @@ const { db } = require("../firebase.js");
 const products = db.collection("products");
 const orders = db.collection("orders");
 const banners = db.collection("banners");
-const users = db.collection("users")
+const users = db.collection("users");
+const cart = db.collection("cart");
 
 module.exports = {
   products,
   orders,
   banners,
-  users
+  users,
+  cart,
 };
