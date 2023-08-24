3e6f7ea9c53fe79d6ab2708d81935e69c6b0cb63
Vansh Singh <vansh2@ualberta.ca>
2023-08-24 15:32:35 +0530

    Added Cart Route

--- src/router/cartRoutes.js
+++ src/router/cartRoutes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const { createCart, getCart } = require("../database/cart");
 const router = express.Router();
+
+// Creating the Cart
+router.post("/", async (req, res) => {
+  const cartData = req.body;
+  const ref = cartData.customer.uid;
+  cartData.ref = ref;
+
+  const newCart = await createCart(cartData);
+
+  res.status(201).send({ status: "OK - Empty Cart Created with UID", data: newCart });
+});
+
+// Get The Cart To Display
 router.get("/:reference", async (req, res) => {
   const cart = await getCart(req.params.reference);
-
+  console.log(cart)
   if (!cart) {
     res.status(404).send({ status: "FAILED", error: "cart not found" });
     return;
@@ -12,9 +25,12 @@ router.get("/:reference", async (req, res) => {
   res.send({ status: "OK", data: cart });
 });
 
+// Update the Cart (Add Product)
 router.put("/:reference/:id", async (req, res) => {
     const cart = await getCart(req.params.reference);
     const product = await getCart(req.params.id);
+    console.log(cart)
+    console.log(product)
   
     if (!cart) {
       res.status(404).send({ status: "FAILED", error: "Cart not found" });
@@ -35,14 +51,5 @@ router.delete("/:reference/clear", async (req, res) => {
     res.send({ status: "OK - Cart Cleared" });
   });
 
-router.post("/", async (req, res) => {
-  const cartData = req.body;
-  const ref = cartData.customer.uid;
-  cartData.ref = ref;
-
-  const newCart = await createCart(cartData);
-
-  res.status(201).send({ status: "OK - Empty Cart Created with User Id", data: newCart });
-});
 
 module.exports = router;
