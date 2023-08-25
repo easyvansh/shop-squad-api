b16088f4657b202e081749cbe1c53f91d8108324
Vansh Singh <vansh2@ualberta.ca>
2023-08-25 13:30:09 +0530

    Updated Cart

--- src/router/cartRoutes.js
+++ src/router/cartRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createCart, getCart,clearCart } = require("../database/cart");
+const { createCart, getCart, clearCart } = require("../database/cart");
 const router = express.Router();
 
 // Creating the Cart
@@ -7,33 +7,18 @@ router.post("/", async (req, res) => {
   const cartData = req.body;
   const ref = cartData.customer.uid;
   cartData.ref = ref;
-  console.log(cartData)
-  try
-  {
-    const cart = await getCart(req.params.reference);
-    const newCart = await createCart(cartData);
-    console.log(cart)
-  if (!cart) {
-    res.status(201).send({ status: "OK - Empty Cart Created with UID", data: newCart });
-  }
-  return;}
-  catch(e){
-    res.status(400).json({
-      error: e.message,
-    });
-    console.log(e)
-  }
-  finally{
-    const newCart = await createCart(cartData);
-  }
+  console.log(cartData);
+  res
+    .status(201)
+    .send({ status: "OK - Empty Cart Created with UID", data: newCart });
 
-}
-);
+  return;
+});
 
 // Get The Cart To Display
 router.get("/:reference", async (req, res) => {
   const cart = await getCart(req.params.reference);
-  console.log(cart)
+  console.log(cart);
   if (!cart) {
     res.status(404).send({ status: "FAILED", error: "cart not found" });
     return;
@@ -43,30 +28,29 @@ router.get("/:reference", async (req, res) => {
 });
 
 // Update the Cart (Add Product)
-router.put("/:reference/:id", async (req, res) => {
-    const cart = await getCart(req.params.reference);
-    const product = await getCart(req.params.id);
-    console.log(cart)
-    console.log(product)
-  
-    if (!cart) {
-      res.status(404).send({ status: "FAILED", error: "Cart not found" });
-      return;
-    }
-  
-    res.send({ status: "OK", data: cart });
-  });
+router.put("/:reference", async (req, res) => {
+  const cart = await getCart(req.params.reference);
+  const product = req.body
+  console.log(cart);
+  console.log(product);
+
+  if (!cart) {
+    res.status(404).send({ status: "FAILED", error: "Cart not found" });
+    return;
+  }
+
+  res.send({ status: "OK", data: cart });
+});
 
 router.delete("/:reference/clear", async (req, res) => {
-    const cart = await clearCart(req.params.reference);
-  
-    if (!cart) {
-      res.status(404).send({ status: "FAILED", error: "cart not found" });
-      return;
-    }
-  
-    res.send({ status: "OK - Cart Cleared" });
-  });
+  const cart = await clearCart(req.params.reference);
 
+  if (!cart) {
+    res.status(404).send({ status: "FAILED", error: "cart not found" });
+    return;
+  }
+
+  res.send({ status: "OK - Cart Cleared" });
+});
 
 module.exports = router;
